ca2556f059c477109d13a46276e9bb024ba8c946
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-02-10 21:56:36 -0500

    crecioon de juego adivina el numero

--- Fundamentos/main.py
+++ Fundamentos/main.py
@@ -1,20 +1,50 @@
+from random import *
+
 #Trabajando con strings
-from posixpath import split
 
-
-mi_text="Mi nombre es, luis"
-result=mi_text[::-1]
+mi_text = "Mi nombre es, luis"
+result = mi_text[::-1]
 #print(result)
 
 #metodos de un strings
-upper= mi_text.upper()
-print(f'texto en mayuscula {upper}')
-lower= mi_text.lower()
-print(f'texto en minuscula {lower}')
-split= mi_text.split(' ')
-print(f'texto separado en comas: {split}')
-join=" ".join(split)
-print(f'texto con join {join}')
+upper = mi_text.upper()
+# print(f'texto en mayuscula {upper}')
+lower = mi_text.lower()
+# print(f'texto en minuscula {lower}')
+split = mi_text.split(' ')
+# print(f'texto separado en comas: {split}')
+join = " ".join(split)
+# print(f'texto con join {join}')
+"""Practicando con ZIP"""
 
-"""Estoy haciendo una prueba de comentario"""
+capitales = ["Berlín", "Tokio", "París", "Helsinki", "Ottawa", "Canberra"]
+paises = ["Alemania", "Japón", "Francia", "Finlandia", "Canadá", "Australia"]
+
+combinado = zip(capitales, paises)
+
+# for capital,pais in combinado:
+# print(f'La capital de {capital} es {pais}')
+"""Practicando con random"""
+colores = ['azul', 'rojo', 'amarillo']
+
+aleatorio = random()
+aleatorioColor=choice(colores)
+
+# print(aleatorioColor)
+"""Compresion de listas"""
+
+palabra='python'
+
+lista= [letra for letra in palabra]
+
+"""PRACTICANDO MATCH en version 3.10"""
+
+serio = "N-02"
+
+match serio:
+    case "N-01":
+        print('samsung')
+    case "N-02":
+        print('nokia')
+
 
--- main.py
+++
@@ -1,5 +0,0 @@
-def print_h1(name):
-    print(f'hi,{name}')
-
-if __name__ == '__main__':
-    print_h1('luis')
